8d5c482011cfe1191b01231038fa72494b642d65
bilu abdi <biluabdi91@gmail.com>
2025-06-09 07:29:46 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function Expression
+const divide = function() {
+  return 2000 / 100;
+};
+
+// Arrow Function with One Parameter
+const square = x => x * x;
+
+// Arrow Function with Two Parameters
+const add = (a, b) => a + b;
